ce868d58c973404ab55ce7f3941f19c4defded1a
Rodolfo Oliveira Miranda <rodolfo.miranda@ourobranco.mg.gov.br>
2024-11-25 07:53:06 -0300

    add function to list customers in memory

--- src/infra/repository/memory/CustomerRepositoryInMemory.ts
+++ src/infra/repository/memory/CustomerRepositoryInMemory.ts
@@ -2,14 +2,18 @@ import { Customer } from "../../../model/Customer";
 import { CustomerRepository } from "../../../repository/CustomerRepository";
 
 export class CustomerRepositoryInMemory implements CustomerRepository {
-    
+
     private customerCollection: Array<Customer>
 
-    constructor(){
+    constructor() {
         this.customerCollection = []
     }
 
     async save(customer: Customer): Promise<void> {
         this.customerCollection.push(customer)
+    }
+
+    async getAll(): Promise<Customer[]> {
+        return this.customerCollection
     }
 }
