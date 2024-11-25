2035a9e08b1b7ebeb03a59575026cb7d49fc4651
Rodolfo Oliveira Miranda <rodolfo.miranda@ourobranco.mg.gov.br>
2024-11-25 07:52:40 -0300

    add get route to list customers

--- src/router.ts
+++ src/router.ts
@@ -1,14 +1,20 @@
 import { Request, Response, Router } from "express"
 import { CustomerRepositoryInMemory } from "./infra/repository/memory/CustomerRepositoryInMemory"
 import { CustomerCreate } from "./controller/CustomerCreate"
+import { CustomerList } from "./controller/CustomerList"
 
 const router = Router()
 
 const repository = new CustomerRepositoryInMemory()
 const customerCreate = new CustomerCreate(repository)
+const customerList = new CustomerList(repository)
 
 router.post('/customer', (request: Request, response: Response) => {
     customerCreate.execute(request, response)
+})
+
+router.get('/customer-list', (request: Request, response: Response) => {
+    customerList.execute(request, response)
 })
 
 export { router }
