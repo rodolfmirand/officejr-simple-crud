ead6f34db7751eda24fb3bb12ebf6a8e231fd1f2
Rodolfo Oliveira Miranda <rodolfo.miranda@ourobranco.mg.gov.br>
2024-11-25 12:38:27 -0300

    add new repository to connect database

---
+++ src/infra/repository/database/CustomerRepositoryDatabase.ts
@@ -0,0 +1,42 @@
+import knex, { Knex } from "knex";
+import { Customer } from "../../../model/Customer";
+import { CustomerRepository } from "../../../repository/CustomerRepository";
+import { config } from '../database/KnexConfig'
+
+export class CustomerRepositoryDatabase implements CustomerRepository {
+
+    private connection: Knex
+
+    constructor() {
+        this.connection = knex(config)
+    }
+
+    async save(customer: Customer): Promise<void> {
+        await this.connection('customer').insert({
+            'id': customer.getId().getValue(),
+            'name': customer.getName(),
+            'document': customer.getDocument().getValue(),
+            'email': customer.getEmail(),
+            'password': customer.getPassword()
+        })
+    }
+
+    async getAll(): Promise<Customer[]> {
+        const customerCollection: Array<Customer> = []
+
+        const customers = await this.connection('customer').select('*')
+
+        for(var c = 0; c < customers.length; c++){
+            const customer = customers[c]
+            const id = customer['id']
+            const name = customer['name']
+            const document = customer['document']
+            const email = customer['email']
+            const password = customer['password']
+
+            customerCollection.push(Customer.createWithId(id, name, document, email, password))
+        } 
+
+        return customerCollection
+    }
+}
