4dbed4c3b7ce63d8129db07d38505dc40a73eac8
Rodolfo Oliveira Miranda <rodolfo.miranda@ourobranco.mg.gov.br>
2024-11-26 08:20:38 -0300

    added function to login

--- src/repository/CustomerRepository.ts
+++ src/repository/CustomerRepository.ts
@@ -3,4 +3,5 @@ import { Customer } from "../model/Customer"
 export interface CustomerRepository {
     save(customer: Customer): Promise<void>
     getAll(): Promise<Array<Customer>>
+    login(email: string, password: string): Promise<boolean>
 }
