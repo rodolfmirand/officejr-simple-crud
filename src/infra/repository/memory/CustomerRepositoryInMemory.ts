a4a3d364419e7619e102b7174e99dfe40972cdd0
Rodolfo Oliveira Miranda <rodolfo.miranda@ourobranco.mg.gov.br>
2024-11-26 08:21:15 -0300

    commented code

--- src/infra/repository/memory/CustomerRepositoryInMemory.ts
+++ src/infra/repository/memory/CustomerRepositoryInMemory.ts
@@ -1,19 +1,19 @@
-import { Customer } from "../../../model/Customer";
-import { CustomerRepository } from "../../../repository/CustomerRepository";
+// import { Customer } from "../../../model/Customer";
+// import { CustomerRepository } from "../../../repository/CustomerRepository";
 
-export class CustomerRepositoryInMemory implements CustomerRepository {
+// export class CustomerRepositoryInMemory implements CustomerRepository {
 
-    private customerCollection: Array<Customer>
+//     private customerCollection: Array<Customer>
 
-    constructor() {
-        this.customerCollection = []
-    }
+//     constructor() {
+//         this.customerCollection = []
+//     }
 
-    async save(customer: Customer): Promise<void> {
-        this.customerCollection.push(customer)
-    }
+//     async save(customer: Customer): Promise<void> {
+//         this.customerCollection.push(customer)
+//     }
 
-    async getAll(): Promise<Customer[]> {
-        return this.customerCollection
-    }
-}
+//     async getAll(): Promise<Customer[]> {
+//         return this.customerCollection
+//     }
+// }
