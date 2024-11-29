7ba7c45090f9e0b8492ef2f75a0bb955a200fed6
Rodolfo Oliveira Miranda <rodolfo.miranda@ourobranco.mg.gov.br>
2024-11-29 07:36:21 -0300

    fixed delete function

--- src/controller/CustomerDelete.ts
+++ src/controller/CustomerDelete.ts
@@ -9,8 +9,10 @@ export class CustomerDelete {
 
     async execute(request: Request, response: Response) {
         const { document } = request.body
-        if (await this.repository.delete(document))
+        if (await this.repository.delete(document)) {
             response.status(200).json('Customer deleted with succeed.')
-        response.status(404).json('Customer not founded.')
+        } else {
+            response.status(404).json('Customer not founded.')
+        }
     }
 }
