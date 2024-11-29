a8a044ed5bbd59813c9555734bcc0f8c12994b2f
Rodolfo Oliveira Miranda <rodolfo.miranda@ourobranco.mg.gov.br>
2024-11-29 07:25:56 -0300

    fixed login function

--- src/controller/CustomerLogin.ts
+++ src/controller/CustomerLogin.ts
@@ -9,8 +9,11 @@ export class CustomerLogin {
 
     async execute(request: Request, response: Response) {
         const { email, password } = request.body
-        if (await this.repository.login(email, password))
+        if (await this.repository.login(email, password)) {
             response.status(200).json('The login has been succeeded.')
-        response.status(401).json('Failed to login.')
+        }
+        else {
+            response.status(401).json('Failed to login.')
+        }
     }
 }
